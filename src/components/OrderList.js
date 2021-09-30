e78d37f39f2667d7be7b0fef07f3be1b37c95647
Joey <87529468+cloudyruer@users.noreply.github.com>
2021-09-30 14:33:24 +0800

    divid to components

--- src/components/OrderList.js
+++ src/components/OrderList.js
@@ -1,7 +1,30 @@
 import React from 'react'
+import ProductItem from './ProductItem'
 
-function OrderList(props) {
-  return <div>OrderList</div>
+function OrderList() {
+  return (
+    <div className="col-md-8 cart">
+      <div className="title">
+        <div className="row">
+          <div className="col">
+            <h4>
+              <b>Shopping Cart</b>
+            </h4>
+          </div>
+          <div className="col align-self-center text-right text-muted">
+            3 items
+          </div>
+        </div>
+      </div>
+      {/*  */}
+      <ProductItem />
+      {/*  */}
+      <div className="back-to-shop">
+        <a href="#/">←</a>
+        <span className="text-muted">Back to shop</span>
+      </div>
+    </div>
+  )
 }
 
 export default OrderList
--- src/components/Summary.js
+++ src/components/Summary.js
@@ -1,7 +1,24 @@
 import React from 'react'
 
 function Summary() {
-  return <div>summary</div>
+  return (
+    <div className="col-md-4 summary">
+      <div>
+        <h5>
+          <b>Summary</b>
+        </h5>
+      </div>
+      <hr />
+      <div className="row">
+        <div className="col col-style">ITEMS 3</div>
+      </div>
+      <div className="row row-style">
+        <div className="col">TOTAL PRICE</div>
+        <div className="col text-right">&euro; 137.00</div>
+      </div>
+      <button className="btn">CHECKOUT</button>
+    </div>
+  )
 }
 
 export default Summary
