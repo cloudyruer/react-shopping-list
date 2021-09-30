005a4d419257eb0809b6ad5763349bb0a2f6aa4c
Joey <87529468+cloudyruer@users.noreply.github.com>
2021-09-30 14:11:10 +0800

    init commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css'
+import OrderList from './components/OrderList'
+import Summary from './components/Summary'
+
+function App() {
+  return (
+    <div>
+      <OrderList />
+      <Summary />
+    </div>
+  )
+}
+
+export default App
